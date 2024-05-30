d748d004390198e04e02b358615a44320864a37a
thienphu <phutot1111@gmail.com>
2024-05-31 01:10:29 +0700

    khoi tao du an fe va be web ban sach

---
+++ web_ban_sach_be/src/main/java/vn/thienphu/web_ban_sach_be/WebBanSachBeApplication.java
@@ -0,0 +1,13 @@
+package vn.thienphu.web_ban_sach_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanSachBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanSachBeApplication.class, args);
+	}
+
+}
---
+++ web_ban_sach_be/src/test/java/vn/thienphu/web_ban_sach_be/WebBanSachBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.thienphu.web_ban_sach_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanSachBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
