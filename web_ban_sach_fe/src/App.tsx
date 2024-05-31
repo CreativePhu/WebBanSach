07b6acb93efdd3bb7294107abb07a9cf8caa6296
thienphu <phutot1111@gmail.com>
2024-05-31 22:36:08 +0700

    them: hoan thanh tao footer cho trang web

--- web_ban_sach_fe/src/App.tsx
+++ web_ban_sach_fe/src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
 
 function App() {
     return (
         <div className={"myApp"}>
             <Navbar/>
+            <Footer/>
         </div>
     );
 }
---
+++ web_ban_sach_fe/src/layout/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className={"container-fluid d-flex align-items-center justify-content-center bg-white"}
+             style={{minHeight: "100px"}}>
+            <span className={"text-muted"}>© 2024 - Design By Ngô Thiên Phú</span>
+        </div>
+    );
+}
