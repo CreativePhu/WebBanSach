d31e6cde1003d86048d2a07b648bd49876998491
thienphu <phutot1111@gmail.com>
2024-06-04 03:19:39 +0700

    cap nhat: responsive co ban phan HersoSection

--- web_ban_sach_fe/src/compoments/items/HeroSection.tsx
+++ web_ban_sach_fe/src/compoments/items/HeroSection.tsx
@@ -15,7 +15,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({listImage, listImage1, listIma
     return (
         <div id={"hero-section"} className="container">
             <div className={"row"}>
-                <div className={"col-8 pe-0 ps-0"}>
+                <div className={"col-lg-12 col-xl-8 pe-0 ps-0"}>
                     <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
                         <div className="carousel-inner">
                             {
@@ -44,7 +44,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({listImage, listImage1, listIma
                         </button>
                     </div>
                 </div>
-                <div className={"col-4 pe-0 d-flex flex-column justify-content-between"}>
+                <div className={"d-none d-xl-block col-xl-4 pe-0 d-flex flex-column justify-content-between"}>
                     {
                         images1.map((image, index) => {
                             return (
