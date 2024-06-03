a18ddf1aea070b9d613a34c2ba9dc465864fc74a
thienphu <phutot1111@gmail.com>
2024-06-04 03:51:42 +0700

    cap nhat: them responsive cho ProductHot

--- web_ban_sach_fe/src/compoments/items/Event.tsx
+++ web_ban_sach_fe/src/compoments/items/Event.tsx
@@ -7,18 +7,20 @@ interface EventProps {
 
 const Event: React.FC<EventProps> = ({listImage}) => {
     return (
-        <div id={"event"} className={"container mt-4 bg-white rounded-4 d-flex flex-row justify-content-around align-items-start py-4"}>
-            {
-                listImage.map((image, index) => {
-                    return (
-                        <div key={index} className={"text-center"} style={{width: "calc(100%/11)"}}>
-                            <img src={image.url} className="rounded mx-auto d-block mb-2" alt="..."
-                                 style={{width: "50px", height: "50px"}}/>
-                            <span>{image.title}</span>
-                        </div>
-                    )
-                })
-            }
+        <div id={"event"} className={"container mt-4 bg-white rounded-4 py-4"}>
+            <div className={"overflow-x-scroll d-flex flex-row justify-content-xl-around justify-content-lg-around hidden-scrollbar"}>
+                {
+                    listImage.map((image, index) => {
+                        return (
+                            <div key={index} className={"d-flex flex-column flex-grow-0 flex-shrink-0 align-items-center justify-content-start text-center me-3 me-xl-0 me-lg-0"} style={{width: "100px"}}>
+                                <img src={image.url} className="rounded mx-auto d-block mb-2" alt="..."
+                                     style={{width: "50px", height: "50px"}}/>
+                                <span>{image.title}</span>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
