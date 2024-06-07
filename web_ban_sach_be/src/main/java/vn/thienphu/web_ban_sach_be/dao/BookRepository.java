12b681d88f44245fa03eeb2e94a743cbb4b4ec30
thienphu <phutot1111@gmail.com>
2024-06-07 22:05:20 +0700

    them: bo sung api tim sach bang bookTitle

--- web_ban_sach_be/src/main/java/vn/thienphu/web_ban_sach_be/dao/BookRepository.java
+++ web_ban_sach_be/src/main/java/vn/thienphu/web_ban_sach_be/dao/BookRepository.java
@@ -1,9 +1,13 @@
 package vn.thienphu.web_ban_sach_be.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 import vn.thienphu.web_ban_sach_be.model.Book;
 
 @RepositoryRestResource(path = "books")
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByBookTitleIsContainingIgnoreCase(@RequestParam("bookTitle") String bookTitle, Pageable pageable);
 }
