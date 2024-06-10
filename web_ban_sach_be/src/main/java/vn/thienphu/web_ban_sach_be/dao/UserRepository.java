08527490169d5eed6f181431619d3a8dcd2374c3
thienphu <phutot1111@gmail.com>
2024-06-10 07:27:43 +0700

    cap nhat: bo sung them api kiem tra ma xac thuc otp bang email va verifyCode

--- web_ban_sach_be/src/main/java/vn/thienphu/web_ban_sach_be/dao/UserRepository.java
+++ web_ban_sach_be/src/main/java/vn/thienphu/web_ban_sach_be/dao/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     User findByUserName(String username);
+
+    boolean existsByEmailAndVerificationCode(String email, String verificationCode);
 }
